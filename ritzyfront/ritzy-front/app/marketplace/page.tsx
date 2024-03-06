26f9c8e3c3754f3b621d4219984984278bc479da
IgnacioNavarro <inavarroblazquez@gmail.com>
2024-03-06 17:12:52 +0100

    project structure

---
+++ ritzyfront/ritzy-front/app/marketplace/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Landing } from "@/components/landing";
+import { Marketplace } from "@/components/marketplace";
+
+
+export default function MarketplacePage() {
+  return (
+    <div>
+      <Marketplace />
+    </div>
+  );
+}
